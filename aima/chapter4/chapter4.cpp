350ddd03f3825260530842cad395d561b11b9905
djlogan2 <djlogan2.dl@gmail.com>
2018-02-09 06:30:17 -0700

    Another chapter4 exercise

--- aima/chapter4/chapter4.cpp
+++ aima/chapter4/chapter4.cpp
@@ -1,4 +1,5 @@
 #include <boost/foreach.hpp>
+#include <algorithm>
 #include <array>
 #include <cmath>
 #include <ctime>
@@ -37,11 +38,70 @@ class Coordinate {
   public:
     Coordinate() { x = 0.0 ; y = 0.0; }
     Coordinate(double x, double y) { this->x = x ; this->y = y; }
+    static Coordinate random() {
+      return Coordinate(std::rand() % 100, std::rand() %100);
+    };
     double x;
     double y;
     friend std::ostream& operator<<(std::ostream& os, const Coordinate& coord) {
       os << '[' << coord.x << ',' << coord.y << ']';
     };
+    Coordinate operator-() {
+      return Coordinate(-x, -y);
+    }
+    Coordinate operator-(const Coordinate& other) {
+      return Coordinate(x-other.x, y-other.y);
+    }
+    Coordinate &operator*=(const double other) {
+      x *= other;
+      y *= other;
+      return *this;
+    }
+    Coordinate operator*(const Coordinate& other) {
+      return Coordinate(x*other.x, y*other.y);
+    }
+    Coordinate operator*(const double value) {
+      return Coordinate(x*value, y*value);
+    }
+    Coordinate operator/(const Coordinate& other) {
+      return Coordinate(x/other.x, y/other.y);
+    }
+    Coordinate operator/(const double value) {
+      return Coordinate(x/value, y/value);
+    }
+    Coordinate &operator/=(const double value) {
+      x /= value;
+      y /= value;
+      return *this;
+    }
+    bool operator<=(const Coordinate& other) {
+      return (x <= other.x && y <= other.y);
+    };
+    bool operator>=(const Coordinate& other) {
+      return (x >= other.x && y >= other.y);
+    };
+    bool operator<=(const double value) {
+      return (x <= value && y <= value);
+    };
+    bool operator>=(const double value) {
+      return (x >= value && y >= value);
+    };
+    bool operator<(const double value) {
+      return (x < value || y < value);
+    };
+    bool operator>(const double value) {
+      return (x > value || y > value);
+    };
+    Coordinate &operator-=(const Coordinate& other) {
+      this->x -= other.x;
+      this->y -= other.y;
+      return *this;
+    }
+    Coordinate &operator+=(const Coordinate& other) {
+      this->x += other.x;
+      this->y += other.y;
+      return *this;
+    }
     Coordinate& operator=(const Coordinate& other) {
       this->x = other.x;
       this->y = other.y;
@@ -49,11 +109,25 @@ class Coordinate {
     }
 };
 
+template<class T, int N>
+std::string sarray(std::array<T, N> array) {
+  std::ostringstream ss;
+  ss << '[';
+  for(int x = 0 ; x < array.size() ; x++)
+    ss << array[x] << ',';
+  ss << ']';
+  return ss.str();
+};
+
 class Map {
   public:
     double total_distance(std::array<Coordinate, 3> airports);
-
+    Coordinate sum();
+    std::array<Coordinate, 3> distance_to_zero(std::array<Coordinate, 3> airports, bool orig=false);
+    int closest_airport(Coordinate city, std::array<Coordinate, 3> airports);
+    std::array<int, 20> closest_airports(std::array<Coordinate, 3> airports);
   private:
+    Coordinate _sum;
     std::array<Coordinate, 20> cities{{
       Coordinate(43,57),
       Coordinate(65,59),
@@ -78,6 +152,95 @@ class Map {
    }};
 };
 
+class {
+  Map m;
+  public:
+    bool operator() (std::array<Coordinate, 3>*s1, std::array<Coordinate, 3>*s2)
+    {
+      if((*s1)[0] == 0) return false;
+      if((*s2)[0] == 0) return true;
+      return m.total_distance(*s1) <= m.total_distance(*s2);
+    }
+} possiblessort;
+
+
+Coordinate Map::sum() {
+  if(_sum.x != 0) return _sum;
+  BOOST_FOREACH(Coordinate city, cities) {
+    _sum += city;
+  };
+  return _sum;
+};
+
+std::array<Coordinate, 3> Map::distance_to_zero(std::array<Coordinate, 3> airports, bool orig) {
+  std::array<Coordinate, 3> sum;
+  std::array<Coordinate, 3> grad;
+  std::array<int, 3> city_count{{0,0,0}};
+  //
+  // Sum the X and Y values for the closest airport to each city
+  //
+  BOOST_FOREACH(Coordinate city, cities) {
+    int which = closest_airport(city, airports);
+    city_count[which]++;
+    sum[which] += city;
+  };
+  //
+  // Now get the zero of the gradient for each airport
+  //
+  for(int i = 0 ; i < 3 ; i++)
+    if(city_count[i])
+      grad[i] = airports[i]-(sum[i]/city_count[i]);
+
+  if(orig) return grad;
+
+  double previous_distance = std::numeric_limits<double>::max();
+  double current_distance = total_distance(airports);
+  double previous_step = 1/std::pow(2,8);
+  double current_step = previous_step;
+  std::array<Coordinate, 3> test;
+  bool outofbounds;
+
+  do {
+    outofbounds = false;
+    previous_distance = current_distance; 
+    previous_step = current_step;
+    current_step *= 2;
+    for(int i = 0 ; i < 3 ; i++) {
+      test[i] = airports[i] - (grad[i]*current_step);
+      if(test[i] < 0 || test[i] > 100)
+        outofbounds = true;
+    };
+    if(!outofbounds)
+      current_distance = total_distance(test);
+    else
+      current_distance = std::numeric_limits<double>::max();
+  } while(current_distance < previous_distance);
+
+  for(int i = 0 ; i < 3 ; i++) grad[i] *= previous_step;
+  return grad;
+};
+
+std::array<int, 20> Map::closest_airports(std::array<Coordinate, 3> airports) {
+  std::array<int, 20> city_count={{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+  for(int x = 0 ; x < 20 ; x++) {
+    city_count[x] = closest_airport(cities[x], airports);
+  };
+  return city_count;
+}
+
+int Map::closest_airport(Coordinate city, std::array<Coordinate, 3> airports) {
+    double minimum_distance = std::numeric_limits<double>::max();
+    int which = std::numeric_limits<int>::max();
+    for(int i = 0 ; i < 3 ; i++) {
+      double single_distance = sqrt(pow(city.x-airports[i].x,2)+pow(city.y-airports[i].y,2));
+      if(minimum_distance > single_distance) {
+        minimum_distance = single_distance;
+        which = i;
+      };
+  };
+  return which;
+}
+
 double Map::total_distance(std::array<Coordinate, 3> airports) {
   double total_distance = 0.0;
   BOOST_FOREACH(Coordinate city, cities) {
@@ -100,19 +263,61 @@ std::array<Coordinate, 3> random_successor(std::array<Coordinate, 3> airports) {
     std::array<Coordinate, 3> successor = airports;
     successor[a].x += x;
     successor[a].y += y;
-    if(successor[a].x >= 0 && successor[a].y >= 0 && successor[a].x < 100 && successor[a].y < 100)
+    if(successor[a]>= 0 && successor[a] <= 100)
       return successor;
   };
 }
 
-std::string sarray(std::array<Coordinate, 3> array) {
-  std::ostringstream ss;
-  ss << '[';
-  for(int x = 0 ; x < 3 ; x++)
-    ss << array[x];
-  ss << ']';
-  return ss.str();
-};
+std::array<Coordinate, 3> gradient_successor(std::array<Coordinate, 3> airports) {
+  Map m;
+  std::array<std::array<Coordinate, 3>, 19> possibles;
+  std::array<Coordinate, 3> dtz = m.distance_to_zero(airports);
+  int psize = 0;
+  for(int a = 0 ; a < 3 ; a++) {
+    for(int x = -1 ; x <= 1 ; x++) {
+      for(int y = -1 ; y <= 1 ; y++) {
+        possibles[psize][a].x = airports[a].x + x;
+        possibles[psize][a].y = airports[a].x + y;
+        psize++;
+      };
+    };
+  };
+
+  possibles[psize] = airports; // Copy over originals in case the gradient if statements only replace part of them
+  //
+  // Now use any gradient minimums we have found. If we have found any, use the ones we found, leaving
+  // any other airports original values if they don't have a valid gradient value. We presume that if
+  // They don't have a valid gradient value, then they are already at their local minimum.
+  //
+  bool gradients = false;
+  bool outofbounds = false;
+  for(int i = 0 ; i < 3 ; i++) {
+    if(std::abs(dtz[i].x) >= 1.0 || std::abs(dtz[i].y) >= 1.0) {
+      Coordinate test = possibles[0][i] - dtz[i];
+      if(test < 0 || test > 100) {
+        outofbounds = true;
+      } else {
+        possibles[psize][i] = test;
+        gradients = true;
+      };
+    };
+  };
+
+  if(gradients && !outofbounds) {
+    int g = psize++;
+    for(int a = 0 ; a < 3 ; a++) {
+      for(int x = -1 ; x <= 1 ; x++) {
+        for(int y = -1 ; y <= 1 ; y++) {
+          possibles[psize][a].x = possibles[g][a].x + x;
+          possibles[psize][a].y = possibles[g][a].x + y;
+          psize++;
+        };
+      };
+    };
+  };
+  std::sort(possibles.begin(), possibles.end(), possiblessort);
+  return possibles[0];
+}
 
 void simulated_annealing() {
   class Map m;
@@ -123,14 +328,15 @@ void simulated_annealing() {
   while((T = schedule) >= 1) {
     if(m.total_distance(current) < m.total_distance(best)) {
       best = current;
-      std::cout << "Saving new best: " << sarray(best) << ", distance=" << m.total_distance(best) << std::endl << std::flush;
+      std::cout << "Saving new best: " << sarray<Coordinate, 3>(best) << ", distance=" << m.total_distance(best) << std::endl << std::flush;
     };
-    std::array<Coordinate, 3> next = random_successor(current);
+
+    std::array<Coordinate, 3> next = gradient_successor(current);
     double td_next = m.total_distance(next);
     double td_current = m.total_distance(current);
     double delta_e = td_current - td_next;
 #if 1
-    std::cout << "Current: " << sarray(current) << ",Next: " << sarray(next) << 
+    std::cout << "Current: " << sarray<Coordinate, 3>(current) << ",Next: " << sarray<Coordinate, 3>(next) << 
                  ",delta: " << delta_e <<
                  ",current dist: " << m.total_distance(current) <<
                  ",next dist: " << m.total_distance(next) <<
@@ -142,12 +348,12 @@ void simulated_annealing() {
 #if 1
       std::cout << " keeping best node" << std::endl << std::flush;
 #endif
-    } else if(std::exp(delta_e/T) >= (double)std::rand() / (double)std::numeric_limits<int>::max()) {
+    } else { //if(std::exp(delta_e/T) >= (double)std::rand() / (double)std::numeric_limits<int>::max()) {
       current = next;
 #if 1
       std::cout << " TRYING worse node" << std::endl << std::flush;
-    } else {
-      std::cout << " not trying worse node" << std::endl << std::flush;
+//    } else {
+//      std::cout << " not trying worse node" << std::endl << std::flush;
 #endif
     };
   };
@@ -160,11 +366,34 @@ void simulated_annealing() {
 }
 
 int main() {
-//  Map m;
-//  std::array<Coordinate, 3> c1{{Coordinate(11,86),Coordinate(23,26),Coordinate(71,74)}};
-//  std::array<Coordinate, 3> c2{{Coordinate(23,24),Coordinate(69,74),Coordinate(12,87)}};
+  Map m;
+//[[11.4943,87.4059][68.7248,73.5887][24.6982,21.5338]]
+//$8 = {x = 10.333333333333334, y = 84}
+//$9 = {x = 72.799999999999997, y = 75.099999999999994}
+//$10 = {x = 21, y = 30}
+//Saving new best: [[10,86][69,74][19,22]], distance=418.728
+  std::array<Coordinate, 3> best1 {{Coordinate(11.4943,87.4059),Coordinate(68.7248,73.5887),Coordinate(24.6982,21.5338)}};
+  std::array<Coordinate, 3> best2 {{Coordinate(11,86),Coordinate(71,74),Coordinate(23,26)}};
+  std::array<Coordinate, 3> best3 {{Coordinate(10,86),Coordinate(69,74),Coordinate(19,22)}};
+  double dist1 = m.total_distance(best1);
+  double dist2 = m.total_distance(best2);
+  double dist3 = m.total_distance(best3);
+  std::array<Coordinate, 3> grad1 = m.distance_to_zero(best1, true);
+  std::array<Coordinate, 3> grad2 = m.distance_to_zero(best2, true);
+  std::array<Coordinate, 3> grad3 = m.distance_to_zero(best3, true);
+  std::array<Coordinate, 3> next1;
+  std::array<Coordinate, 3> next2;
+  std::array<Coordinate, 3> next3;
+  for(int i = 0 ; i < 3 ; i++) next1[i] = best1[i] - grad1[i];
+  for(int i = 0 ; i < 3 ; i++) next2[i] = best2[i] - grad2[i];
+  for(int i = 0 ; i < 3 ; i++) next3[i] = best3[i] - grad3[i];
+  double dist4 = m.total_distance(next1);
+  double dist5 = m.total_distance(next2);
+  double dist6 = m.total_distance(next3);
 //  std::cout << "c1=" << m.total_distance(c1) << std::endl;
 //  std::cout << "c2=" << m.total_distance(c2) << std::endl;
+//  exit(0);
+//  std::array<Coordinate, 6> minimum = m.minimum(std::array<Coordinate, 3>{{Coordinate(22,26), Coordinate(68,73), Coordinate(10,86)}});
 //  exit(0);
   std::srand(std::time(NULL));
   simulated_annealing();
---
+++ aima/chapter4/chapter4b.cpp
@@ -0,0 +1,205 @@
+#include <array>
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include <sstream>
+#include <vector>
+
+#define STOP  0
+#define LEFT  1
+#define UP    2
+#define RIGHT 4
+#define DOWN  8
+
+class Map {
+  private:
+    std::array<int, 9> map;
+    int current_location;
+    int goal;
+    int _moves;
+
+  public:
+    bool won() { return current_location == goal; }
+    int at() { return current_location; }
+    int moves() { return _moves; }
+    std::queue<int> available_actions() {
+      std::queue<int> ret;
+      for(int x = 8 ; x != 0 ; x >>= 1) {
+        if(map[current_location] & x)
+          ret.push(x);
+      };
+      return ret;
+    }
+    Map() {
+      current_location = 2;
+      goal = 3;
+      _moves = 0;
+      map = {{
+        RIGHT,    LEFT+DOWN,     DOWN,
+        DOWN,     UP+DOWN,       UP+DOWN,
+        UP+RIGHT, LEFT+UP+RIGHT, UP+LEFT
+      }};
+    }
+
+    int reverse_move(int move) {
+      switch(move) {
+        default:
+          throw "Invalid move";
+        case UP:
+          return DOWN;
+        case DOWN:
+          return UP;
+        case LEFT:
+          return RIGHT;
+        case RIGHT:
+          return LEFT;
+      };
+      return current_location;
+    };
+
+    int move(int _move) {
+      switch(map[current_location] & _move) {
+        case STOP:
+          throw "Invalid move";
+        case UP:
+          current_location -= 3;
+          break;
+        case DOWN:
+          current_location += 3;
+          break;
+        case LEFT:
+          current_location -= 1;
+          break;
+        case RIGHT:
+          current_location += 1;
+          break;
+      };
+      _moves++;
+      return current_location;
+    };
+};
+
+class hashing_func
+{
+  public:
+    unsigned long operator()(const std::array<int, 2> &key) const {
+      return key[0] * 100 + key[1];
+    };
+};
+class key_equal_func
+{
+  public:
+    bool operator()(const std::array<int, 2> &a, const std::array<int, 2> &b) const {
+      return a[0] == b[0] && a[1] == b[1];
+    };
+};
+
+Map m;
+std::unordered_map<int, std::queue<int>> untried;
+std::unordered_map<std::array<int, 2>, int, hashing_func, key_equal_func> results;
+std::unordered_map<int, std::queue<int>> unbacktracked;
+
+int previous_state = -1;
+int previous_action = -1;
+
+std::string dir(int action) {
+  switch(action) {
+    case STOP: return "stop";
+    case UP: return "UP";
+    case DOWN: return "DOWN";
+    case LEFT: return "LEFT";
+    case RIGHT: return "RIGHT";
+  };
+}
+
+std::string print_untried() {
+  std::ostringstream ss;
+  ss << "untried: [";
+  for(auto iter : untried) {
+    ss << '[' << iter.first << '=';
+    std::queue<int> temp = iter.second;
+    while(!temp.empty()) { ss << dir(temp.front()) << ','; temp.pop(); }
+    ss << ']';
+  };
+  ss << ']';
+  return ss.str();
+}
+
+std::string print_results() {
+  std::ostringstream ss;
+  ss << "results: [";
+  for(auto iter : results) {
+    auto key = iter.first;
+    auto action = iter.second;
+    ss << "move " << dir(action) << " from " << key[0] << " to " << key[1] << ',';
+  };
+  ss << ']';
+  return ss.str();
+}
+
+std::string print_unbacktracked() {
+  std::ostringstream ss;
+  ss << "unbacktracked: [";
+  for(auto iter : unbacktracked) {
+    ss << '[' << iter.first << '=';
+    std::queue<int> temp = iter.second;
+    while(!temp.empty()) { ss << temp.front() << ','; temp.pop(); }
+    ss << ']';
+  };
+  ss << ']';
+  return ss.str();
+}
+
+int online_dfs_agent() {
+
+  if(m.won()) {
+    std::cout << "We have reached the goal" << std::endl << std::flush;
+    return STOP;
+  };
+
+  auto available_actions = untried.find(m.at());
+  if(available_actions == untried.end()) {
+    untried[m.at()] = m.available_actions();
+  };
+
+  if(previous_state != -1) {
+    results[{{previous_state, m.at()}}] = previous_action;
+    results[{{m.at(), previous_state}}] = m.reverse_move(previous_action);
+//    std::queue<int>::iterator it = find(unbacktracked[m.at()].begin(), unbacktracked[m.at()].end(), previous_action);
+//    if(it == unbacktracked[m.at()].end())
+      unbacktracked[m.at()].push(previous_state);
+  };
+
+  std::cout << print_untried() << std::endl << std::flush;
+  std::cout << print_unbacktracked() << std::endl << std::flush;
+  std::cout << print_results() << std::endl << std::flush;
+
+  if(!untried.size() || !untried[m.at()].size()) {
+    if(!unbacktracked.size() || !unbacktracked[m.at()].size()) return STOP;
+    int undo_state = unbacktracked[m.at()].front();
+    unbacktracked[m.at()].pop();
+    //if(!unbacktracked[m.at()].size()) unbacktracked.erase(m.at());
+    int undo_action = results[{{m.at(), undo_state}}];
+    std::cout << "Backtracking from " << m.at() << " to " << undo_state << std::endl << std::flush;
+    return undo_action;
+  } else {
+    int action = untried[m.at()].front();
+    untried[m.at()].pop();
+    // Don't do this! if(!untried[m.at()].size()) untried.erase(m.at());
+    std::cout << "Executing new action " << dir(action) << std::endl << std::flush;
+    return action;
+  };
+}
+
+int main() {
+  int action = online_dfs_agent();
+  while(action != STOP) {
+    previous_state = m.at();
+    previous_action = action;
+    std::cout << "Making move #" << m.moves() << ' ' << dir(action) << " from " << m.at();
+    m.move(action);
+    std::cout << " to " << m.at() << std::endl << std::endl << std::flush;
+    action = online_dfs_agent();
+  };
+  return 0;
+}
